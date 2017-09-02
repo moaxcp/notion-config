1ec5c66b83e2a64c2f6a35fcd19389e6af31b92f
John Mercier <moaxcp@gmail.com>
2017-09-01 22:58:19 -0400

    adding files from newer version of notion

--- notion/cfg_tiling.lua
+++ notion/cfg_tiling.lua
@@ -2,29 +2,28 @@
 -- Notion tiling module configuration file
 --
 
--- Bindings for the tilings. 
+-- Bindings for the tilings.
 defbindings("WTiling", {
-    bdoc("Go to frame right of current frame."),
-    kpress(META.."Right", "ioncore.goto_next(_sub, 'right')"),
-    bdoc("Go to frame left of current frame."),
-    kpress(META.."Left", "ioncore.goto_next(_sub, 'left')"),
-    bdoc("Go to frame above current frame."),
-    kpress(META.."Up", "ioncore.goto_next(_sub, 'up', {no_ascend=_})"),
-    bdoc("Go to frame below current frame."),
-    kpress(META.."Down", "ioncore.goto_next(_sub, 'down', {no_ascend=_})"),
-
     bdoc("Split current frame vertically."),
-    kpress(META.."bracketright", "WTiling.split_at(_, _sub, 'bottom', true)"),
-    bdoc("Split current frame horizontally."),
-    kpress(META.."bracketleft", "WTiling.split_at(_, _sub, 'right', true)"),
-    
-    bdoc("Split screen vertically."),
-    kpress(META.."Shift+bracketright", "WTiling.split_top(_, 'bottom')"),
-    bdoc("Split screen horizontally."),
-    kpress(META.."Shift+bracketleft", "WTiling.split_top(_, 'right')"),
+    kpress(META.."S", "WTiling.split_at(_, _sub, 'bottom', true)"),
 
-    bdoc("Destroy current frame."),
-    kpress(META.."Shift+apostrophe", "WTiling.unsplit_at(_, _sub)"),
+    bdoc("Go to frame above/below current frame."),
+    kpress(META.."P", "ioncore.goto_next(_sub, 'up', {no_ascend=_})"),
+    kpress(META.."N", "ioncore.goto_next(_sub, 'down', {no_ascend=_})"),
+    mclick(META.."Shift+Button4", "ioncore.goto_next(_sub, 'up', {no_ascend=_})"),
+    mclick(META.."Shift+Button5", "ioncore.goto_next(_sub, 'down', {no_ascend=_})"),
+
+    bdoc("Go to frame right/left of current frame."),
+    kpress(META.."Tab", "ioncore.goto_next(_sub, 'right')"),
+    submap(META.."K", {
+        kpress("Tab", "ioncore.goto_next(_sub, 'left')"),
+
+        bdoc("Split current frame horizontally."),
+        kpress("S", "WTiling.split_at(_, _sub, 'right', true)"),
+
+        bdoc("Destroy current frame."),
+        kpress("X", "WTiling.unsplit_at(_, _sub)"),
+    }),
 })
 
 -- Frame bindings.
@@ -37,23 +36,23 @@ defbindings("WFrame.floating", {
 
 -- Context menu for tiled workspaces.
 defctxmenu("WTiling", "Tiling", {
-    menuentry("Destroy frame", 
+    menuentry("Destroy frame",
               "WTiling.unsplit_at(_, _sub)"),
 
-    menuentry("Split vertically", 
+    menuentry("Split vertically",
               "WTiling.split_at(_, _sub, 'bottom', true)"),
-    menuentry("Split horizontally", 
+    menuentry("Split horizontally",
               "WTiling.split_at(_, _sub, 'right', true)"),
-    
+
     menuentry("Flip", "WTiling.flip_at(_, _sub)"),
     menuentry("Transpose", "WTiling.transpose_at(_, _sub)"),
-    
+
     menuentry("Untile", "mod_tiling.untile(_)"),
-    
+
     submenu("Float split", {
-        menuentry("At left", 
+        menuentry("At left",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'left')"),
-        menuentry("At right", 
+        menuentry("At right",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'right')"),
         menuentry("Above",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'up')"),
@@ -62,16 +61,16 @@ defctxmenu("WTiling", "Tiling", {
     }),
 
     submenu("At root", {
-        menuentry("Split vertically", 
+        menuentry("Split vertically",
                   "WTiling.split_top(_, 'bottom')"),
-        menuentry("Split horizontally", 
+        menuentry("Split horizontally",
                   "WTiling.split_top(_, 'right')"),
         menuentry("Flip", "WTiling.flip_at(_)"),
         menuentry("Transpose", "WTiling.transpose_at(_)"),
     }),
 })
 
--- Extra context menu extra entries for floatframes. 
+-- Extra context menu extra entries for floatframes.
 defctxmenu("WFrame.floating", "Floating frame", {
     append=true,
     menuentry("New tiling", "mod_tiling.mkbottom(_)"),
